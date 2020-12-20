bf1c16129d2d1bb79526a3094cd9063f918f90ff
Sergei Cherepanov <s.cherepanov@gmail.com>
2020-12-20 23:23:02 +0300

    Import

---
+++ app.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from filelock import FileLock
+from flask import Flask, request, send_file
+
+app = Flask(__name__)
+
+DATABASE_PATH = 'data/temperatures.csv'
+
+
+@app.route('/temperature', methods=['PUT'])
+def put_temperature():
+    content = request.json
+    temperatures = content['temp_sma']
+    timestamp = datetime.now().timestamp()
+    rows = ''.join([f'{timestamp},{clock_time},{temp}\n' for clock_time, temp in temperatures.items()])
+    with FileLock(f'{DATABASE_PATH}.lock', timeout=1):
+        with open(DATABASE_PATH, 'at') as f:
+            f.write(rows)
+    return 'Ok!'
+
+
+@app.route('/csv', methods=['GET'])
+def get_csv():
+    return send_file(DATABASE_PATH)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
