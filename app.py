d52e8f13ed017aa2d33a72c98e9a027c65622893
Sergei Cherepanov <s.cherepanov@gmail.com>
2020-12-25 18:45:58 +0300

    Use influxdb instead of csv

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
-from filelock import FileLock
-from flask import Flask, request, send_file
+from dateutil import tz
+from flask import Flask, request
+from influxdb import InfluxDBClient
 
 app = Flask(__name__)
-
-DATABASE_PATH = 'data/temperatures.csv'
-DATABASE_LOCK = FileLock(f'{DATABASE_PATH}.lock', timeout=1)
+influxdb = InfluxDBClient(database='temperatures')
 
 
 @app.route('/temperature', methods=['PUT'])
 def put_temperature():
-    content = request.json
-    temperatures = content['temp_sma']
-    timestamp = datetime.now().timestamp()
-    rows = ''.join([f'{timestamp},{clock_time},{temp}\n' for clock_time, temp in temperatures.items()])
-    with DATABASE_LOCK:
-        with open(DATABASE_PATH, 'at') as f:
-            f.write(rows)
+    temperatures = sorted(request.json['median_temps'].items(), key=lambda x: x[0], reverse=True)
+    now = datetime.now().timestamp()
+
+    latest_clock_time = None
+    data_points = []
+    for key, temperature in temperatures:
+        epoch, clock_time = key.split('_')
+        clock_time = int(epoch) * (2 ** 32) + int(clock_time)
+        if latest_clock_time is None:
+            latest_clock_time = clock_time
+        timestamp = int(now * 1000 - (latest_clock_time - clock_time))
+        data_points.append(f'temperature value={temperature} {timestamp}')
+
+    influxdb.write_points(data_points, protocol='line', time_precision='ms')
     return 'Ok!'
 
 
-@app.route('/csv', methods=['GET'])
-def get_csv():
-    with DATABASE_LOCK:
-        return send_file(DATABASE_PATH, 'text/csv')
+@app.route('/status', methods=['GET'])
+def check():
+    timestamp_str = next(influxdb.query('SELECT * FROM temperature ORDER BY time DESC LIMIT 1').get_points())['time']
+    timestamp = datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
+    time_passed = datetime.now().astimezone(tz.tzlocal()) - timestamp
+    if time_passed > timedelta(minutes=15):
+        return 'Temperature readings are stale', 500
+    return f'Ok (last data received {time_passed.seconds // 60}m ago)'
 
 
 if __name__ == '__main__':
